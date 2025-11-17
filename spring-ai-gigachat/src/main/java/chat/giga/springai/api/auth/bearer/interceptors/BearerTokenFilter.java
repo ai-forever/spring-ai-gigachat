eb459732c8cf0f32b8856460e4ebdeefeb834a11
Ravil Zakirov <ravilzzakirov@gmail.com>
2025-11-17 14:41:05 +0300

    Merge fix

--- spring-ai-gigachat/src/main/java/chat/giga/springai/api/auth/bearer/interceptors/BearerTokenFilter.java
+++ spring-ai-gigachat/src/main/java/chat/giga/springai/api/auth/bearer/interceptors/BearerTokenFilter.java
@@ -37,10 +37,6 @@ public record BearerTokenFilter(ApiKey tokenRenewer) implements ExchangeFilterFu
     @Override
     @SuppressWarnings("NullableProblems")
     public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
-        String token = tokenRenewer.getValue();
-
-        if (token == null || token.isEmpty()) return next.exchange(request);
-
         return next.exchange(ClientRequest.from(request)
                 .header(AUTHORIZATION, "Bearer " + tokenRenewer.getValue())
                 .build());
