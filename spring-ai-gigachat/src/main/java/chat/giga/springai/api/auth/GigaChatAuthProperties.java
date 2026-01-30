e3b2c712aa533f2070511c89c7f671ade57aa848
Linar Abzaltdinov <abzaltdinov@gmail.com>
2026-01-30 10:50:26 +0300

    remove code duplication
    Signed-off-by: Linar Abzaltdinov <abzaltdinov@gmail.com>

--- spring-ai-gigachat/src/main/java/chat/giga/springai/api/auth/GigaChatAuthProperties.java
+++ spring-ai-gigachat/src/main/java/chat/giga/springai/api/auth/GigaChatAuthProperties.java
@@ -95,9 +95,6 @@ public class GigaChatAuthProperties {
             if (StringUtils.hasText(this.apiKey)) {
                 return this.apiKey;
             }
-            if (StringUtils.hasText(this.apiKey)) {
-                return this.apiKey;
-            }
             if (StringUtils.hasText(this.clientId) && StringUtils.hasText(this.clientSecret)) {
                 return HttpHeaders.encodeBasicAuth(this.clientId, this.clientSecret, StandardCharsets.UTF_8);
             }
